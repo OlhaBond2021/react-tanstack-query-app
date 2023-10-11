672c790dc820e9bf15fd6ee318b7c7234b01dae7
OlhaBond2021 <96439245+OlhaBond2021@users.noreply.github.com>
2023-10-11 14:10:11 +0300

    another approach - use react router + react query

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,49 +1,62 @@
-import { Link, useNavigate, useParams } from "react-router-dom";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import {
+  Link,
+  redirect,
+  useNavigate,
+  useParams,
+  useSubmit,
+  useNavigation,
+} from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
 import { fetchEvent, updateEvent, queryClient } from "../../util/http";
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
-import LoadingIndicator from "../UI/LoadingIndicator";
 import ErrorBlock from "../UI/ErrorBlock";
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const { state } = useNavigation();
+  const submit = useSubmit();
   const params = useParams();
 
-  const { data, isPending, isError, error } = useQuery({
+  const { data, isError, error } = useQuery({
     queryKey: ["events", params.id],
     queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+    staleTime: 10000,
   });
 
-  const { mutate } = useMutation({
-    mutationFn: updateEvent,
-    //маніпуляція з кешованими даними, яку автоматично робить react query, а зараз робимо вручну
-    onMutate: async (data) => {
-      // data - дані, які змінив користувач і відправив на бекенд(тобто це formData)
-      const newEvent = data.event;
-      // скасування всіх активних запитів до певного ключа, щоб не було конфліктів з оптимістично оновленими даними (наступна строка)
-      await queryClient.cancelQueries({ queryKey: ["events", params.id] });
-      const previousEvent = queryClient.getQueryData(["events", params.id]); //зберігаємо старі данні на випадок виникнення помилки зі сторони бека
+  // const { mutate } = useMutation({
+  //   mutationFn: updateEvent,
+  //   //маніпуляція з кешованими даними, яку автоматично робить react query, а зараз робимо вручну
+  //   onMutate: async (data) => {
+  //     // data - дані, які змінив користувач і відправив на бекенд(тобто це formData)
+  //     const newEvent = data.event;
+  //     // скасування всіх активних запитів до певного ключа, щоб не було конфліктів з оптимістично оновленими даними (наступна строка)
+  //     await queryClient.cancelQueries({ queryKey: ["events", params.id] });
+  //     const previousEvent = queryClient.getQueryData(["events", params.id]); //зберігаємо старі данні на випадок виникнення помилки зі сторони бека
 
-      queryClient.setQueryData(["events", params.id], newEvent); // оптимістично оновлені дані
+  //     queryClient.setQueryData(["events", params.id], newEvent); // оптимістично оновлені дані
 
-      return { previousEvent };
-    },
+  //     return { previousEvent };
+  //   },
 
-    onError: (error, data, context) => {
-      // context contain previousEvent
-      queryClient.setQueryData(["events", params.id], context.previousEvent);
-    },
-    onSettled: () => {
-      //onSettled буде викликана щоразу коли буде виконуватись mutationFn, не залежно від відповіді (error or success)
-      queryClient.invalidateQueries(["events", params.id]); //гарантує отримання останніх змін (з бека)
-    },
-  });
+  //   onError: (error, data, context) => {
+  //     // context contain previousEvent
+  //     queryClient.setQueryData(["events", params.id], context.previousEvent);
+  //   },
+  //   onSettled: () => {
+  //     //onSettled буде викликана щоразу коли буде виконуватись mutationFn, не залежно від відповіді (error or success)
+  //     queryClient.invalidateQueries(["events", params.id]); //гарантує отримання останніх змін (з бека)
+  //   },
+  // });
+
+  // function handleSubmit(formData) {
+  //   mutate({ id: params.id, event: formData });
+  //   navigate("../");
+  // }
 
   function handleSubmit(formData) {
-    mutate({ id: params.id, event: formData });
-    navigate("../");
+    submit(formData, { method: "PUT" });
   }
 
   function handleClose() {
@@ -51,13 +64,6 @@ export default function EditEvent() {
   }
 
   let content;
-  if (isPending) {
-    content = (
-      <div className="center">
-        <LoadingIndicator />
-      </div>
-    );
-  }
 
   if (isError) {
     content = (
@@ -81,14 +87,38 @@ export default function EditEvent() {
   if (data) {
     content = (
       <EventForm inputData={data} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
+        {state === "submitting" ? (
+          <p>Sending data...</p>
+        ) : (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Update
+            </button>
+          </>
+        )}
       </EventForm>
     );
   }
   return <Modal onClose={handleClose}>{content}</Modal>;
+}
+
+// перед відображенням компонента EditEvent запуститься loader, тому isPending прибрали з компонента
+export function loader({ params }) {
+  //fetchQuery автоматично кешує отримані дані
+  return queryClient.fetchQuery({
+    queryKey: ["events", params.id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+  });
+}
+
+//ця функція буде виконана коли відбудеться відправка форми
+export async function action({ request, params }) {
+  const formData = await request.formData();
+  const updatedEventData = Object.fromEntries(formData);
+  await updateEvent({ id: params.id, event: updatedEventData });
+  await queryClient.invalidateQueries(["events"]); // в цьому випадку не буде оптимістично оновлені дані, це інший підхід
+  return redirect("../");
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
+import { useIsFetching } from "@tanstack/react-query";
+
 export default function Header({ children }) {
+  //fetching === 0 якщо не відправляє жодного запиту, або більше число якщо отримує якісь дані
+  const fetching = useIsFetching();
+
   return (
     <>
-      <div id="main-header-loading"></div>
+      <div id="main-header-loading">{fetching > 0 && <progress />}</div>
       <header id="main-header">
         <div id="header-title">
           <h1>React Events</h1>
