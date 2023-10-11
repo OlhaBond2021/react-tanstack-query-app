b540129b87b33b7f2a78d8cf8d8238463e032cea
OlhaBond2021 <96439245+OlhaBond2021@users.noreply.github.com>
2023-10-11 12:50:05 +0300

    add edit fn

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,20 +1,86 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
+import { fetchEvent, updateEvent, queryClient } from "../../util/http";
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
+import LoadingIndicator from "../UI/LoadingIndicator";
+import ErrorBlock from "../UI/ErrorBlock";
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const params = useParams();
 
-  function handleSubmit(formData) {}
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events", params.id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+    //маніпуляція з кешованими даними, яку автоматично робить react query, а зараз робимо вручну
+    onMutate: async (data) => {
+      // data - дані, які змінив користувач і відправив на бекенд(тобто це formData)
+      const newEvent = data.event;
+      // скасування всіх активних запитів до певного ключа, щоб не було конфліктів з оптимістично оновленими даними (наступна строка)
+      await queryClient.cancelQueries({ queryKey: ["events", params.id] });
+      const previousEvent = queryClient.getQueryData(["events", params.id]); //зберігаємо старі данні на випадок виникнення помилки зі сторони бека
+
+      queryClient.setQueryData(["events", params.id], newEvent); // оптимістично оновлені дані
+
+      return { previousEvent };
+    },
+
+    onError: (error, data, context) => {
+      // context contain previousEvent
+      queryClient.setQueryData(["events", params.id], context.previousEvent);
+    },
+    onSettled: () => {
+      //onSettled буде викликана щоразу коли буде виконуватись mutationFn, не залежно від відповіді (error or success)
+      queryClient.invalidateQueries(["events", params.id]); //гарантує отримання останніх змін (з бека)
+    },
+  });
+
+  function handleSubmit(formData) {
+    mutate({ id: params.id, event: formData });
+    navigate("../");
+  }
 
   function handleClose() {
     navigate("../");
   }
 
-  return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
+  let content;
+  if (isPending) {
+    content = (
+      <div className="center">
+        <LoadingIndicator />
+      </div>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <>
+        <ErrorBlock
+          title="Failed to load event"
+          massage={
+            error.info?.message ||
+            "Failed to load event. Please check your inputs and try again later."
+          }
+        />
+        <div className="form-actions">
+          <Link to="../" className="button">
+            Okay
+          </Link>
+        </div>
+      </>
+    );
+  }
+
+  if (data) {
+    content = (
+      <EventForm inputData={data} onSubmit={handleSubmit}>
         <Link to="../" className="button-text">
           Cancel
         </Link>
@@ -22,6 +88,7 @@ export default function EditEvent() {
           Update
         </button>
       </EventForm>
-    </Modal>
-  );
+    );
+  }
+  return <Modal onClose={handleClose}>{content}</Modal>;
 }
